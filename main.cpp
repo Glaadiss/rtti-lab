2d542ffa68a1c544165b10f6b318f87aecc8ddec
Glaadiss <bartekgladys@gmail.com>
2018-02-27 18:02:34 +0100

    Add classes and randomAnimal() method

--- main.cpp
+++ main.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+class Animal {
+public:
+    virtual ~Animal() {}
+    virtual void speak() const = 0;
+};
+
+class Cat : public Animal {
+public:
+    virtual void speak() const { std::cout << "Miau!" << std::endl; }
+};
+
+class Dog : public Animal {
+public:
+    virtual void speak() const { std::cout << "Hau!" << std::endl; }
+};
+
+class Tiger : public Cat {
+public:
+    virtual void speak() const { std::cout << "mraU!" << std::endl; }
+};
+
+Animal* randomAnimal(){
+    switch (rand() % 3){
+        case 0:
+            return new Cat();
+        case 1:
+            return new Dog();
+        case 2:
+            return new Tiger();
+    }
+    return nullptr;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
