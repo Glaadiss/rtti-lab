a5834e0046c6944b4fde4363314548c54f322f89
Glaadiss <bartekgladys@gmail.com>
2018-02-27 17:00:25 +0100

    Generate project in clion

---
+++ cmake-build-debug/CMakeFiles/rtti_lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtti_lab.dir/main.cpp.o"
+  "rtti_lab.pdb"
+  "rtti_lab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtti_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
